0b4c9c7f4f91274cd940e64def122efe936e1cc6
Riddle Hsu <riddlehsu@google.com>
2020-02-22 23:20:41 +0800

    RESTRICT AUTOMERGE Create separated tasks for different apps from startActivities
    Assume there are 2 applications A, B with different uids.
    There are 4 activities A1, A2, B1, B2 with default task
    affinity and launch mode.
    
    After A1 called startActivities(B1, A2, B2):
     Original   : Task(A1, B1, A2, B2)
     This Change: Task(A1, B1), Task(A2, B2)
    In other words, the source caller cannot launch its activity
    above the activity of other application in the same task, and
    it can still launch activity of other application in its task.
    
    Bug: 145669109
    Test: run cts --test android.server.cts.StartActivityTests \
          -m CtsServicesHostTestCases
    
    [basilgello: Back-ported to 14.1:
     - Added definition for ActivityRecord.getUid() from
       fwb/82ea6cb9de38fd20ebcddfd84f0132908beeeab1]
    Signed-off-by: Vasyl Gello <vasek.gello@gmail.com>
    
    Change-Id: I97bd875146a52f62b8fe82235487ccefb2955e8e
    (cherry picked from commit 2be3ba49733df585bcfbf63d3b299d15dbfb0f13)

--- services/core/java/com/android/server/am/ActivityRecord.java
+++ services/core/java/com/android/server/am/ActivityRecord.java
@@ -1557,6 +1557,10 @@ final class ActivityRecord {
         }
     }
 
+    int getUid() {
+        return info.applicationInfo.uid;
+    }
+
     @Override
     public String toString() {
         if (stringName != null) {
